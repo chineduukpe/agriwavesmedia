321bc678d30bb67e1fe8b67d4b0d024f14692a17
Chinedu Ukpe <cukpe@emgeresources.com>
2021-08-26 10:20:25 +0100

    Design

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agriwaves Media</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <p><i class="fa fa-phone"></i> +2348038989764, +2347099857485</p>
+    </header>
+    <nav>
+        <a href="javascript:;" class="mobile-nav-toggle"><i class="fa fa-bars"></i></a>
+        <div class="brand">
+            <img src="./img/logo.png" alt="">
+        </div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">What we do</a></li>
+            <li><a href="/team.html">Team</a></li>
+            <li><a href="">Contact us </a></li>
+            <li><a href="/blogs.html">Blog</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section class="banner">
+            <div class="text-wrapper">
+                <hgroup>
+                    <h3>Hello. You’re welcome to</h3>
+                    <h1>AGRIWAVES MEDIA WEBSITE</h1>
+                    <!-- <h3>Africa’s Leading Agricultural Communications Hub</h3> -->
+                    <p>…promoting African Agriculture</p>
+                    <a href="#" class=" btn primary ">Learn more...</a>
+                </hgroup>
+            </div>
+            <div class="right-side"></div>
+
+            <div class="banner-blog">
+                <img src="img/bg5.jpg" alt="">
+                <h3 class="blog-title"><a href="#2">Building a new capacity</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed veritatis fugit, aperiam obcaecati voluptatem minima n</p>
+                <span class="author">--Felix Nyikwagh</span>
+            </div>
+            <div class="banner-blog">
+                <img src="img/bg4.jpg" alt="">
+                <h3 class="blog-title"><a href="#3">Having true mind for look</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed veritatis fugit, aperiam obcaecati voluptatem minima n</p>
+                <span class="author">--Kate Habilla</span>
+            </div>
+            <div class="banner-blog">
+                <img src="img/bg3.jpg" alt="">
+                <h3 class="blog-title"><a href="#4">Blue oak trees thriving</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed veritatis fugit, aperiam obcaecati voluptatem minima</p>
+                <span class="author">--Denis White</span>
+            </div>
+        </section>
+        <section class="scene what-we-do">
+            <h2>What <span>We do</span></h2>
+            <p class="subtitle-paragraph">We provide technical and strategic communication innovations for enhanced development opportunities in food and agriculture. To meet our vision, we act as a catalyst, capacity builder and enabler for deploying a genuinely demand-driven approach in our programming. Agriwaves Media amplifies and transforms African Agriculture to evolve sustainable development and a food secure continent.</p>
+            <div class="service-list">
+                <div class="service-item">
+                    <span class="faded-title">1</span>
+                    <h3><i class="fa fa-user-circle-o"></i> Social and Behavioural Change Communication (SBCC)</h3>
+                    <p>Our team is experienced in designing tools and implementing change projects in a range of development spectrum; nutrition, food and agriculture etc.</p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="service-item">
+                    <span class="faded-title">2</span>
+                    <h3><i class="fa fa-tty"></i>Advocacy Communication and Social Mobilization (ACSM)</h3>
+                    <p>We are good at Advocacy and Stakeholder Engagement.</p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="service-item">
+                    <span class="faded-title">3</span>
+                    <h3><i class="fa fa-commenting"></i>Media Communications and Management</h3>
+                    <p>We understand the media environment and how best to incorporate vast experience in content development for strategic communications and engagement.</p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="service-item">
+                    <span class="faded-title">4</span>
+                    <h3>Multimedia Productions and Documentation:</h3>
+                    <p>We use creative tools to capture project and campaign efforts and gather feedback and impact through a variety of platforms.  </p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="service-item">
+                    <span class="faded-title">5</span>
+                    <h3><i class="fa fa-image"></i>Graphics Design:</h3>
+                    <p>We create highly engaging infographics, pictograms, manuals, brochures, illustrations; IEC materials, flyers and reports with user-friendly formats.  </p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="service-item">
+                    <span class="faded-title">6</span>
+                    <h3><i class="fa fa-image"></i>Videography and Photography:</h3>
+                    <p>We produce compelling pictures and videos that resonate with their vision of our clients; we highlight the emotions that matter.  Our products include; documentary and short films  </p>
+                    <a href="/about.html">Read more <i class="fa fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </section>
+
+        <section class="projects scene">
+            <h3 class="floating-title">Gallery</h3>
+            <h2 class='text-center uppercase heading thin'>Our <span class="text-primary strong">Gallery</span></h2>
+            <p class="text-center text-muted subtitle-paragraph">Here are some pictures from our gallery. Our happy clients approved showcasing this on our site.</p>
+            <div class="home-gallery-wrapper">
+                <div class="gallery-item">
+                    <img src="img/bg1.webp" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg3.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg4.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg5.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg6.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg3.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg3.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <div class="gallery-item">
+                    <img src="img/bg3.jpg" alt="">
+                    <span class="gallery-title">Hover mane lake</span>
+                </div>
+                <a href="" class="btn btn-lg center">More from our gallery</a>
+            </div>
+        </section>
+
+        <section class="home-recent-posts scene">
+            
+            <h2 class='text-center uppercase heading thin'>Recent Blog <span class="text-primary strong">Posts</span></h2>
+            <div class="featured-blog">
+                <span class="tag">Fish traingin</span>
+                <div class="img-section">
+                    <img src="./img/bg4.jpg" alt="">
+                </div>
+                <div class="text-content">
+                    
+                    <span class="meta"><i class="fa fa-clock-o"></i> 22/03/2021</span>
+                    <h4>Building the next generation food security...</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aliquid voluptatem qui iusto quidem, doloremque suscipit ex labore, officiis, laudantium rem repellendus. A, repellat architecto. Suscipit animi rem minima eum....</p>
+                    <div class="author">
+                        <img src="./img/index.png" alt="" class="thumbnail">
+                        <span>Chinedu Ukpe</span>
+                    </div>
+                </div>
+            </div>
+            <div class="blog-list">
+                <div class="blog">
+                    <span class="tag">Fish traingin</span>
+                    <img src="./img/bg6.jpg" alt="">
+                    <span class="meta"><i class="fa fa-clock-o"></i>  23/09/2020</span>
+                    <h4>Hover and the two main threads</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam alias amet porro eum saepe molestiae itaque mollitia earum dolore rerum quasi doloribus sapiente, esse fuga natus suscipit recusandae! Explicabo, quo?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam alias amet porro eum saepe molestiae itaque mollitia earum dolore rerum quasi doloribus sapiente, esse fuga natus suscipit recusandae! Explicabo, quo?</p>
+                </div>
+                <div class="blog">
+                    <img src="./img/bg3.jpg" alt="">
+                    <span class="meta"><i class="fa fa-clock-o"></i>  23/09/2020</span>
+                    <h4>Hover and the two main threads</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam alias amet porro eum saepe molestiae itaque mollitia earum dolore rerum quasi doloribus sapiente, esse fuga natus suscipit recusandae! Explicabo, quo?</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam alias amet porro eum saepe molestiae itaque mollitia earum dolore rerum quasi doloribus sapiente, esse fuga natus suscipit recusandae! Explicabo, quo?</p>
+                </div>
+            </div>
+            <a href="/blogs.html" class="btn center btn-lg">Blogs</a>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+
+
+    <script src="https://use.fontawesome.com/c4c1bccc3a.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.1/gsap.min.js"></script>
+    <script>
+        const banner_blogs = document.querySelectorAll('.banner-blog');
+
+        let current_banner_blog = 0;
+        const banner_blogs_count = banner_blogs.length;
+        banner_blogs[0].classList.add('show');
+
+        setInterval(() => {
+            current_banner_blog = (current_banner_blog + banner_blogs_count + 1) % banner_blogs_count;
+            [].forEach.call(banner_blogs, (blog, index) => {
+                if (index === current_banner_blog) {
+                    return blog.classList.add('show');
+                }
+                return blog.classList.remove('show')
+            })
+        }, 7000)
+
+
+        const nav = document.querySelector('nav')
+
+        /* Handle scroll */
+        window.addEventListener('scroll', e => {
+            console.log(pageYOffset);
+            if (pageYOffset > 100) {
+                return nav.classList.add('bg-white')
+            }
+            return nav.classList.remove('bg-white');
+        })
+
+        const mobile_nav_toggle = document.querySelector('.mobile-nav-toggle');
+        const nav_list = document.querySelector('nav ul')
+
+        mobile_nav_toggle.addEventListener('click', e => {
+            e.target.classList.toggle('active')
+            nav_list.classList.toggle('mobile-active')
+        })
+
+        /* end handle scroll */
+
+        /* GSAP ANIMATIONS */
+        gsap.from('.banner .text-wrapper h3', 2,{
+            y: 100,
+        })
+        gsap.from('.banner .text-wrapper h1', 2,{
+            y: 100,
+            height: 0,
+        })
+        gsap.from('.banner .text-wrapper p', 2,{
+            x: 100,
+        })
+        window.onload = function(){
+
+        }
+        /* END GSAP ANIMATIONS */
+    </script>
+</body>
+</html>
